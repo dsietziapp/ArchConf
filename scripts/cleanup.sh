3f5de63297c5be4249a665dfb3b2baf14da2d3f2
dsietziapp <dsietz@iapp.org>
2020-11-03 12:09:04 -0500

    working on scripts

---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Cleaning up ...
+
+cd $HOME/environment
+rm -f kafka_2.13-2.6.0.tgz
--- scripts/workshop.sh
+++ scripts/workshop.sh
@@ -2,20 +2,25 @@
 cd $HOME/environment
 
 echo Downloading scripts ...
-aws s3 cp s3://iapp-archconf-workshop/install-rust.sh
-aws s3 cp s3://iapp-archconf-workshop/install-kafka.sh
-aws s3 cp s3://iapp-archconf-workshop/kafka-start.sh
-aws s3 cp s3://iapp-archconf-workshop/kafka-stop.sh
-aws s3 cp s3://iapp-archconf-workshop/zookeeper-start.sh
-aws s3 cp s3://iapp-archconf-workshop/zookeeper-stop.sh
-sudo chmod +x install-rust.sh
-sudo chmod +x install-kafka.sh
-sudo chmod +x kafka-start.sh
-sudo chmod +x kafka-stop.sh
-sudo chmod +x zookeeper-start.sh
-sudo chmod +x zookeeper-stop.sh
+files=( "install-rust.sh" "install-kafka.sh" "kafka-start.sh" "kafka-stop.sh" "zookeeper-start.sh" "zookeeper-stop.sh" "cleanup.sh" )
+for f in "${files[@]}"
+do
+    :
+    aws s3 cp s3://iapp-archconf-workshop/$f $f
+    sudo chmod +x $f
+done
 
-echo Performing installations ...
-./install-rust.sh
-source $HOME/.cargo/env
-./install-kafka.sh
+installs=( "install-rust.sh" "install-kafka.sh" )
+for i in "${installs[@]}"
+do
+    :
+    echo "running $i ..."
+    ./$i
+done
+
+./cleanup.sh
+
+#echo Performing installations ...
+#./install-rust.sh
+#source $HOME/.cargo/env
+#./install-kafka.sh
