230021a21bce6cf475cf3bfdf3611aee67c69ddb
dsietziapp <52455684+dsietziapp@users.noreply.github.com>
2020-11-03 08:44:53 -0500

    Initialize sandbox

---
+++ .github/workflows/branch-build.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#CONSTANTS
+export BUILD_NUMBER=${GITHUB_WORKFLOW// /-}-$GITHUB_RUN_NUMBER
+echo "Build Number: $BUILD_NUMBER"
+
+#FUNCTIONS
+set_cft_dir() {
+   if [ -z "$IAPP_pipeline_build_dir" ]
+   then
+      echo "Using the default directory cloudformation ..."   
+      CFT_DIR="cloudformation"
+   else
+      echo "Using configured build directory $IAPP_pipeline_build_dir ..."
+      CFT_DIR=$IAPP_pipeline_build_dir
+   fi
+}
+
+
+###################### 
+# Script starts here #
+######################
+# include common shared functions
+. "$WORKFLOW_HOME/common.sh"
+# First, get the configurations settings
+parse_property_files $BRANCH
+
+# determine the release level
+case $BRANCH in
+    development)
+        echo "Using the development aws account for branch $BRANCH ..."
+        ARTIFACT_MATURITY="alpha"
+        ;;
+    test)
+        echo "Using the test aws account for branch $BRANCH ..."
+        ARTIFACT_MATURITY="beta"
+        ;;
+    master)
+        echo "Using the master aws account for branch $BRANCH ..."
+        ARTIFACT_MATURITY="general-release"
+        ;;
+    *)
+        echo "Using sandbox aws account for branch $BRANCH ..."
+        ARTIFACT_MATURITY="pre-alpha"
+        BRANCH="sandbox"
+        ;;
+esac
+
+assume_role $IAPP_aws_account $DEPLOY_ROLE
+
+case $ARTIFACT_MATURITY in 
+    pre-alpha)
+        # build from scratch 
+        build_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $BUILD_NUMBER $IAPP_pipeline_release_vrs
+        deploy_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $IAPP_pipeline_release_vrs
+        ;;
+    alpha)
+        # build from scratch
+        build_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $BUILD_NUMBER $IAPP_pipeline_release_vrs
+        deploy_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $IAPP_pipeline_release_vrs
+        ;;
+    beta)
+        # promote alpha to beta and test and then to release candidate
+        promote_artifacts "alpha" "beta" $IAPP_pipeline_release_vrs
+        echo "   testing succesfully completed ..."
+        echo "   promoting artifacts to release candidates ..."
+        promote_artifacts "beta" "release-candidate" $IAPP_pipeline_release_vrs
+        deploy_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $IAPP_pipeline_release_vrs
+        ;;
+    general-release)
+        echo "   promoting artifacts to general release ..."
+        promote_artifacts "release-candidate" "general-release" $IAPP_pipeline_release_vrs
+        deploy_templates $IAPP_aws_account $DEPLOY_ROLE $IAPP_pipeline_build_dir $IAPP_aws_region $IAPP_pipeline_release_vrs
+        ;;
+    *)
+        echo "Error: $ARTIFACT_MATURITY is an unsupported build level!"
+        exit 1
+        ;;
+esac
+
+echo "Done ..."
---
+++ README.md
@@ -0,0 +1,18 @@
+# cft-project
+![Production CFT Project](https://github.com/PrivacyAssociation/cft-project/workflows/Production%20Pipeline/badge.svg)
+
+![Nonproduction CFT Project](https://github.com/PrivacyAssociation/cft-project/workflows/Nonproduction%20Pipeline/badge.svg)
+
+---
+
+This is a boilerplate for creating AWS Cloudformation repositories for a product. It includes an example [Cloudformaiton Template (CFT)](./cloudformation/template.yaml) as well as an automated CI/CD pipeline for building and deploying the specified templates.
+
+**IMPORTNAT** Make sure to modify the properties files in the `.iapp` folder to 
+
+---
+
+### BRANCHING
++ [Branching Model](./BRANCHING.md)
+
+### PIPELINE
++ [CI/CD Pipeline](./PIPELINE.md)
