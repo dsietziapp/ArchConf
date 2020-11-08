4f17932b4d5556d1cbd874a46924184db78102c2
dsietziapp <dsietz@iapp.org>
2020-11-08 13:56:17 -0500

    working on provisioning scripts

--- scripts/curl-sourcing.sh
+++ scripts/curl-sourcing.sh
@@ -6,6 +6,7 @@ curl --location --request POST 'http://localhost:8000/order/clothing/iStore/5000
 --header 'Data-Tracker-Chain: W3siaWRlbnRpZmllciI6eyJkYXRhX2lkIjoib3JkZXJ+Y2xvdGhpbmd+aVN0b3JlfjUwMDAiLCJpbmRleCI6MCwidGltZXN0YW1wIjowLCJhY3Rvcl9pZCI6IiIsInByZXZpb3VzX2hhc2giOiIwIn0sImhhc2giOiI3MjI1OTUwMzMyNzI3NjAyMDk1MjEwMjM2ODY3MjE0ODM1ODQ4NSIsIm5vbmNlIjo1fV0=' \
 --header 'Authorization: Basic aXN0b3JlX2FwcDpzZWNyZXQ=' \
 --data-raw '{
+	"product":"leather jacket",
 	"quantity": 1,
 	"status":"new"
 }'
