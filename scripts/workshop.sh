6a023ee2d417483762a8144c8d25c1d3cf95ed71
dsietziapp <dsietz@iapp.org>
2020-11-09 10:03:55 -0500

    add  curl-reporting.sh

--- scripts/workshop.sh
+++ scripts/workshop.sh
@@ -5,7 +5,7 @@ echo Updating yum ...
 sudo yum update -y
 
 echo Downloading scripts ...
-files=( "install-rust.sh" "install-kafka.sh" "kafka-start.sh" "kafka-stop.sh" "zookeeper-start.sh" "zookeeper-stop.sh" "resize.sh" "cleanup.sh" "curl-hello.sh" "curl-sourcing.sh" )
+files=( "install-rust.sh" "install-kafka.sh" "kafka-start.sh" "kafka-stop.sh" "zookeeper-start.sh" "zookeeper-stop.sh" "resize.sh" "cleanup.sh" "curl-hello.sh" "curl-sourcing.sh" "curl-reporting.sh" )
 for f in "${files[@]}"
 do
     :
