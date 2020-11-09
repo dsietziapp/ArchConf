fe88e48a80c9fe059417bfc428f258c03ee9fad1
dsietziapp <dsietz@iapp.org>
2020-11-09 10:29:03 -0500

    working on curl-reporitng.sh

--- scripts/curl-reporting.sh
+++ scripts/curl-reporting.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
-curl --location --request GET 'http://localhost:8001/all'
+PRODUCT=all
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        -p|--product)
+        shift
+        PRODUCT=$*
+        PRODUCT="${PRODUCT// /_}"
+        ;;
+    esac
+done
+curl --location --request GET "http://localhost:8001/${PRODUCT}"
