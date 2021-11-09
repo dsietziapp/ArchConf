0b516122cb9b2451e86bf37c4bf59d193bdb1b4f
dsietziapp <52455684+dsietziapp@users.noreply.github.com>
2021-11-09 14:13:16 -0500

    Update install-rust.sh

--- scripts/daas-workshop/install-rust.sh
+++ scripts/daas-workshop/install-rust.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo Installing Rust ...
 cd $HOME/environment
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-aws s3 cp s3://iapp-archconf-workshop/rustup-init.sh rustup-init.sh
-sudo chmod +x rustup-init.sh
-./rustup-init.sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+#aws s3 cp s3://iapp-archconf-workshop/rustup-init.sh rustup-init.sh
+#sudo chmod +x rustup-init.sh
+#./rustup-init.sh -y
 export PATH=$PATH:$HOME/.cargo/bin
 source $HOME/.cargo/env
 rustup default stable
